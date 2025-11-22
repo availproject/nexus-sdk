74a8b252ab9f55f183b99c6e52564347a242c5a7
Abhishek <abhishek.chaudhary.2008@gmail.com>
2025-11-22 21:42:16 +0400

    fix: removed telemetry for now, fix: deadline removed from approval for now, fixed approval deadline usage in SBC

--- src/sdk/ca-base/nexusError.ts
+++ src/sdk/ca-base/nexusError.ts
@@ -1,5 +1,3 @@
-import { AnyValueMap, SeverityNumber } from '@opentelemetry/api-logs';
-import telemetryLogger from '../telemetry';
 export interface NexusErrorData {
   context?: string; // Where or why it happened
   cause?: unknown; // Optional nested error
@@ -57,16 +55,6 @@ export type ErrorCode = (typeof ERROR_CODES)[keyof typeof ERROR_CODES];
 
 export function createError(code: ErrorCode, message: string, data?: NexusErrorData): NexusError {
   const nexusError = new NexusError(code, message, data);
-  telemetryLogger.emit({
-    body: message,
-    severityNumber: SeverityNumber.ERROR,
-    severityText: 'ERROR',
-    attributes: {
-      data: nexusError.data,
-      cause: code,
-      stackTrace: nexusError.stack,
-    } as AnyValueMap,
-  });
   return nexusError;
 }
 
