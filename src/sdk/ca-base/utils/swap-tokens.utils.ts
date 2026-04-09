4543410b032930ed3275a4683f11724d2bcd8046
Abhishek <abhishek.chaudhary.2008@gmail.com>
2026-04-09 16:14:37 +0400

    fix: add logo urls for wcbtc & ctusd token

--- src/sdk/ca-base/utils/swap-tokens.utils.ts
+++ src/sdk/ca-base/utils/swap-tokens.utils.ts
@@ -1,6 +1,6 @@
 // This file contains extra token details which are not supported for bridging but should be used for multicall chain balances
 
-import { MAINNET_CHAIN_IDS, type TokenInfo } from '../../../commons';
+import { MAINNET_CHAIN_IDS, NEXUS_ASSETS_BASE_URL, type TokenInfo } from '../../../commons';
 
 type TokenByChain = Array<{
   chainId: number;
@@ -14,14 +14,14 @@ export const TOKENS_BY_CHAIN: TokenByChain = [
       {
         contractAddress: '0x3100000000000000000000000000000000000006',
         name: 'Wrapped Citrea Bitcoin',
-        logo: '',
+        logo: `${NEXUS_ASSETS_BASE_URL}/wcbtc/logo.png`,
         decimals: 18,
         symbol: 'WCBTC',
       },
       {
         contractAddress: '0x8D82c4E3c936C7B5724A382a9c5a4E6Eb7aB6d5D',
         name: 'Citrea USD',
-        logo: '',
+        logo: `${NEXUS_ASSETS_BASE_URL}/ctusd/logo.png`,
         decimals: 6,
         symbol: 'ctUSD',
       },
