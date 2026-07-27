4c5cf5c04bd11af10fbf90bf5303eabf949afea5
Abhishek <Makyl@users.noreply.github.com>
2026-07-27 16:35:33 +0400

    example: update browser app to React Router 8 (#223)

--- example/browser/src/main.tsx
+++ example/browser/src/main.tsx
@@ -7,7 +7,7 @@ import "@fontsource/geist-mono";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import type { Transport } from "viem";
 import { http, WagmiProvider } from "wagmi";
 import {
